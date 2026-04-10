8f94ed33e9fa186691bd198d38b93317416fa18b
XUAN LIU <142984388+lmsbd@users.noreply.github.com>
2026-04-10 11:12:31 +1000

    deploy

--- Backend/src/main/resources/sql/init.sql
+++ Backend/src/main/resources/sql/init.sql
@@ -1,8 +1,4 @@
-Drop Database IF EXISTS rfo_db;
--- Create database
-CREATE DATABASE IF NOT EXISTS rfo_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE rfo_db;
 
 -- ============================================
 -- User table
